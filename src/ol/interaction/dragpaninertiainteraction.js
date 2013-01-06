3e5a98dd8d5e853a59bacce88c4f00f13c87485c
Andrey Geonya <manufacturer.software@gmail.com>
2013-01-06 23:44:08 +0200

    inertia 90% done

--- src/ol/interaction/dragpaninertiainteraction.js
+++ src/ol/interaction/dragpaninertiainteraction.js
@@ -12,12 +12,12 @@ ol.interaction.DragPanInertia = function(condition) {
   goog.base(this, condition);
 
   // requestAnimationFrame shim with setTimeout fallback
-  window.requestAnimFrame = 
-            window.requestAnimationFrame       || 
-            window.webkitRequestAnimationFrame || 
-            window.mozRequestAnimationFrame    || 
-            window.oRequestAnimationFrame      || 
-            window.msRequestAnimationFrame     || 
+  window.requestAnimFrame =
+            window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
             function( callback ){
               window.setTimeout(callback, 1000 / 60);
             };
@@ -49,11 +49,13 @@ ol.interaction.DragPanInertia.prototype.handleDragEnd = function(mapBrowserEvent
   };
   this._vector1 = {
     x1: 0, y1: 0,
-    x2: 0, y2: 0
+    x2: 0, y2: 0,
+    a: 0, b: 0
   };
   this._vector2 = {
     x1: 0, y1: 0,
-    x2: 0, y2: 0
+    x2: 0, y2: 0,
+    a: 0, b: 0
   };
 
   var self = this;
@@ -61,13 +63,65 @@ ol.interaction.DragPanInertia.prototype.handleDragEnd = function(mapBrowserEvent
 
   var inertiaMove = function() {
 
+    var isStopNextFrame = false;
+
+    // vector 1 start
+    self._vector1.x1 = map.getCenter().x;
+    self._vector1.y1 = map.getCenter().y;
+
     var newCenter = new ol.Coordinate(
       map.getCenter().x - self._impulse.x,
       map.getCenter().y + self._impulse.y
     );
-    map.setCenter(newCenter);
-    window.requestAnimFrame(inertiaMove);
 
+    map.setCenter(newCenter);
+
+    // vector 1 end, vector 2 start
+    self._vector1.x2 = self._vector2.x1 = newCenter.x;
+    self._vector1.y2 = self._vector2.y1 = newCenter.y;
+  
+    // decrease impulse
+    self._impulse.x /= ol.interaction.DragPanInertia.SLOWING_SPEED;
+    self._impulse.y /= ol.interaction.DragPanInertia.SLOWING_SPEED;
+
+    // vector 2 end
+    self._vector2.x2 = newCenter.x - self._impulse.x;
+    self._vector2.y2 = newCenter.y + self._impulse.y;
+
+    // vector 1 a and b, @see http://www.math.by/geometry/eqline.html
+    self._vector1.a = self._vector1.y1 - self._vector1.y2;
+    self._vector1.b = self._vector1.x2 - self._vector1.x1;
+
+    // vector 2 a and b
+    self._vector2.a = self._vector2.y1 - self._vector2.y2;
+    self._vector2.b = self._vector2.x2 - self._vector2.x1;
+
+    // if vectors are not parallels, @see http://bit.ly/KmdcJH
+    if((self._vector1.a * self._vector2.b - self._vector2.a * self._vector1.b) !== 0) {
+
+      // calculate moving corner, @see http://www.math.by/geometry/anglline.html
+      var corner = Math.acos(
+        (self._vector1.b * self._vector2.b + self._vector1.a * self._vector2.a) /
+        ( Math.sqrt(Math.pow(self._vector1.b, 2) + Math.pow(self._vector1.a, 2)) *
+          Math.sqrt(Math.pow(self._vector2.b, 2) + Math.pow(self._vector2.a, 2)) ));
+      corner *= (180 / Math.PI);
+
+      // if moving corner changed more than ol.interaction.DragPanInertia.STOP_CORNER,
+      // then stop moving
+      if(corner >= ol.interaction.DragPanInertia.STOP_CORNER) {
+        isStopNextFrame = true;
+      }
+
+    }
+
+    if (self._impulse.x <= ol.interaction.DragPanInertia.STOP_IMPULSE &&
+        self._impulse.y <= ol.interaction.DragPanInertia.STOP_IMPULSE) {
+      isStopNextFrame = true;
+    }
+
+    if (isStopNextFrame === false) {
+      window.requestAnimFrame(inertiaMove);
+    }
   };
 
   inertiaMove();
@@ -76,9 +130,19 @@ ol.interaction.DragPanInertia.prototype.handleDragEnd = function(mapBrowserEvent
 /**
  * @const {number}
  */
-ol.interaction.DragPanInertia.MASS = 180000;
+ol.interaction.DragPanInertia.MASS = 300000;
 
 /**
  * @const {number}
  */
-ol.interaction.DragPanInertia.SLOWING_SPEED = 1.2;
+ol.interaction.DragPanInertia.SLOWING_SPEED = 1.1;
+
+/**
+ * @const {number}
+ */
+ol.interaction.DragPanInertia.STOP_CORNER = 40;
+
+/**
+ * @const {number}
+ */
+ol.interaction.DragPanInertia.STOP_IMPULSE = 600;
