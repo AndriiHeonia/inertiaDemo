669d3d78cfe23dd900c4d85e661818ec0ba24969
Andrey Geonya <manufacturer.software@gmail.com>
2013-01-07 14:26:17 +0200

    ol removed

--- ol3-inertia-drag/loader.js
+++
@@ -1,4 +0,0 @@
-(function() {
-  document.write('<link rel="stylesheet" href="ol.css" type="text/css">');
-  document.write('<script type="text/javascript" src="compile.js"></script>');
-}());
--- src/ol/renderer/dom/domrenderer.js
+++
@@ -1,9 +0,0 @@
-goog.provide('ol.renderer.dom');
-
-goog.require('goog.functions');
-
-
-/**
- * @return {boolean} Is supported.
- */
-ol.renderer.dom.isSupported = goog.functions.TRUE;
